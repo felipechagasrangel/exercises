ff7a5574589b4b4d26b6ffed8880a74862bc3da9
Felipe Rangel <felipechagasrangel@gmail.com>
2024-08-17 21:18:38 -0300

    fix: mean from 1 to 100 considering all cases

--- giant_steps_15/src/main.py
+++ giant_steps_15/src/main.py
@@ -6,16 +6,27 @@ from concurrent.futures import ProcessPoolExecutor
 
 def find_coin_quantity_for_each_type(coins_config: List[int], value: int):
     """ Finds the quantity of coins, given a configuration, needed to represent the value """
-    coin_quantity = {key: 0 for key in coins_config}
 
-    k = value
-    coin_index = 0
-    while k > 0:
-        if k >= coins_config[coin_index]:
-            k -= coins_config[coin_index]
-            coin_quantity[coins_config[coin_index]] += 1
-        else:
-            coin_index += 1
+    coins_less_than_value = [coin for coin in coins_config if value >= coin]
+    quantity_of_coins_less_than_value = len(coins_less_than_value)
+    sub_coins_config = []
+    for quantity in range(quantity_of_coins_less_than_value):
+        sub_coins_config.append(coins_less_than_value[quantity:])
+
+    coin_quantity = None
+    for sub_config in sub_coins_config:
+        sub_coin_quantity = {key: 0 for key in sub_config}
+        k = value
+        coin_index = 0
+        while k > 0:
+            if k >= sub_config[coin_index]:
+                k -= sub_config[coin_index]
+                sub_coin_quantity[sub_config[coin_index]] += 1
+            else:
+                coin_index += 1
+
+        if coin_quantity is None or sum([coin_quantity[key] for key in coin_quantity]) > sum([sub_coin_quantity[key] for key in sub_coin_quantity]):
+            coin_quantity = sub_coin_quantity.copy()
 
     return coin_quantity
 
@@ -31,7 +42,7 @@ def mean_of_coins_quantity(coins_config: List[int]) -> float:
         coin_quantity = find_coin_quantity_for_each_type(coins_config, k)
         quantity_of_coins[k] = (sum([coin_quantity[key] for key in coin_quantity]))
 
-    return sum([quantity_of_coins[key] for key in quantity_of_coins]) / 100
+    return sum([quantity_of_coins[key] for key in quantity_of_coins]) / 99
 
 
 def construct_new_coins_config(coins_config: List[int], included_coins: Tuple[int]):
@@ -65,15 +76,17 @@ def parallel_min(*args):
 
     inclusions, coins_config, number_of_steps = args
     min_mean = 100
-    min_config = None
+    min_config = []
 
     inclusion_index = 0
     for inclusion in inclusions:
         new_coins_config = construct_new_coins_config(coins_config, inclusion)
         min_mean_candidate = mean_of_coins_quantity(new_coins_config)
         if min_mean_candidate < min_mean:
-            min_config = new_coins_config.copy()
+            min_config = [new_coins_config.copy()]
             min_mean = min_mean_candidate
+        elif min_mean_candidate < min_mean:
+            min_config.append(new_coins_config.copy())
         if inclusion_index == number_of_steps:
             break
         inclusion_index += 1
@@ -119,19 +132,22 @@ def find_best_inclusion(number_of_threads: int, coins_config: List[int], how_man
 if __name__ == "__main__":
     number_of_threads = 6
 
-    # a) 2
+    # x_coins_config = [10, 7, 1]
+    # find_coin_quantity_for_each_type(x_coins_config, 14)
+
+    # a) [100, 50, 43, 25, 10, 5, 1]
     a_coins_config = [100, 50, 25, 10, 5, 1]
     a_how_many_coins = 1
     a_min_mean, a_min_candidate = find_best_inclusion(number_of_threads, a_coins_config, a_how_many_coins)
     print(f"a) The minimum is {a_min_mean} with configuration {a_min_candidate}")
 
-    # b) 2 e 37
+    # b) [100, 50, 43, 25, 10, 5, 2, 1]
     b_coins_config = [100, 50, 25, 10, 5, 1]
     b_how_many_coins = 2
     b_min_mean, b_min_candidate = find_best_inclusion(number_of_threads, b_coins_config, b_how_many_coins)
     print(f"b) The minimum is {b_min_mean} with configuration {b_min_candidate}")
 
-    # c) [45, 18, 7, 3, 1] e 3.5
+    # c) [46, 29, 8, 3, 1] e 3.44
     c_coins_config = []
     c_how_many_coins = 5
     c_min_mean, c_min_candidate = find_best_inclusion(number_of_threads, c_coins_config, c_how_many_coins)
